4c4f48abebe3152c5d21a8fe5feebb07759995a2
Stefano Ginella <stefano@stefanoginella.com>
2024-09-09 11:54:49 +0200

    Added is_plugin_active check for Woptimize and admin notice

--- wop-responsive-block-settings.php
+++ wop-responsive-block-settings.php
@@ -16,20 +16,50 @@
  *
  */
 
-// Abort if accessing the file directly
+// Abort if accessing the file directly.
 defined( 'ABSPATH' ) || exit;
 
-// Define useful constants
-define( 'WOP_RBS_URL', plugin_dir_url( __FILE__ ) );
-define( 'WOP_RBS_DIR', plugin_dir_path( __FILE__ ) );
-define( 'WOP_RBS_VER', '1.0.1' );
 
-// Includes
-$includes = [
-	'render-block',
-	'rest-api',
-	'scripts',
-];
-foreach ( $includes as $include ) {
-	require_once( WOP_RBS_DIR . "includes/$include.php" );
+// To detect plugin also in the frontend.
+include_once ABSPATH . 'wp-admin/includes/plugin.php';
+// Load this plugin only if Woptimize is not active.
+if ( ! is_plugin_active( 'woptimize/woptimize.php' ) ) {
+
+	// Define useful constants.
+	define( 'WOP_RBS_URL', plugin_dir_url( __FILE__ ) );
+	define( 'WOP_RBS_DIR', plugin_dir_path( __FILE__ ) );
+	$plugin_data = get_file_data( __FILE__, array( 'Version' => 'Version' ) );
+	define( 'WOP_RBS_VER', $plugin_data[ 'Version' ] );
+
+	// Includes.
+	$includes = [
+		'render-block',
+		'rest-api',
+		'scripts',
+	];
+	foreach ( $includes as $include ) {
+		require_once( WOP_RBS_DIR . "includes/$include.php" );
+	}
 }
+
+
+
+/**
+ * Display an admin notice if Woptimize is installed.
+ * The Responsive Block Settings plugin is not necessary if Woptimize is active.
+ *
+ * @return void
+ */
+function wop_plugin_notice() {
+    if ( is_plugin_active( 'woptimize/woptimize.php' ) ) { ?>
+        <div class="notice notice-info">
+            <p>Please, to avoid possible conflicts deactivate and uninstall <strong>Responsive Block Settings</strong>. The same functionalitites are provided by <strong>Woptimize</strong>. Go to the <a href="<?php echo admin_url('plugins.php') ?>">Plugins page</a> to deactivate it.</p>
+			<p><?php printf(
+				__( 'Please, to avoid possible conflicts deactivate and uninstall <strong>Responsive Block Settings</strong>. The same functionalities are provided by <strong>Woptimize</strong>. Go to the <a href="%s">Plugins page</a> to deactivate it.', 'wop-responsive-block-settings' ),
+				esc_url( admin_url( 'plugins.php' ) )
+			); ?></p>
+        </div>
+    <?php
+    }
+}
+add_action( 'admin_notices', 'wop_plugin_notice' );
